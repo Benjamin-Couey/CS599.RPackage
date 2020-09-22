9a8ab0d1afb04be15d0df680ad93aa03f4dd5f9a
Benjamin Couey <bhc36@nau.edu>
2020-09-22 15:21:20 -0600

    Initial commit of boilerplate code and function definitions

---
+++ R/HCLUST.R
@@ -0,0 +1,76 @@
+# Description - Performs basic hierarchical clustering using single linkage.
+#               Computes it's own distance matrix.
+# Arguments - data.matrix : a numeric data matrix to perform clustering on
+#             K : the number of clusters
+# Returns - a vector with as many elements as data.matrix has rows where each
+#           element indicate which cluster the row at that index in the
+#           data.matrix is assigned
+HCLUST <- function( data.matrix, K ){
+
+  diss.matrix <- matrix( nrow=nrow( data.matrix ), ncol=nrow( data.matrix ) )
+
+  # First, compute a dissimilarity matrix for data.matrix
+  for( outer.i in 1:(nrow(data.matrix)) ) {
+    # We can assume the distance between a row and itself is 0
+    diss.matrix[ outer.i, outer.i ] <- 0
+
+    if( outer.i < nrow(data.matrix) ) {
+
+      for( inner.i in (outer.i+1):nrow(data.matrix) ) {
+        # Calculate the Euclidian distance between the first vector and the second
+        distance <- sqrt( sum( ( data.matrix[outer.i,] - data.matrix[inner.i,] )^2 ) )
+        # Assign it to the positions in the matrix which are diagonally symmetrical
+        diss.matrix[outer.i, inner.i] <- distance
+        diss.matrix[inner.i, outer.i] <- distance
+      }
+
+    }
+
+  }
+
+  # Set the diagonal of the distance matrix to NA since we don't care about
+  # comparing points to themselves.
+  diag(diss.matrix)=NA
+
+  # Initialize a list which will store the assignments of rows in the data.matrix
+  # to clusters
+  cluster.list <- as.list( 1:nrow(diss.matrix) )
+
+  while( length(cluster.list) > K ){
+
+    # Find the shortest distance in the distance matrix, ignoring NA entires
+    # which are either the distance of a point to itself, or the distance of a
+    # point to another point in the same cluster
+    shortest <- NULL
+    shortest <- which(diss.matrix == min(diss.matrix [ !is.na(diss.matrix) ]),
+                      arr.ind = TRUE)
+
+    to.keep.i <- shortest[1,1]
+    to.remove.i <- shortest[1,2]
+
+    # Recalculate the distance matrix
+
+    # Take the min of the joined vectors in the data matrix
+    # This minimum vector represents the distance to the new cluster using the
+    # single linkage method
+    min.vector <- pmin( diss.matrix[ to.keep.i, ], diss.matrix[ to.remove.i, ] )
+
+    diss.matrix[ to.keep.i, ] <- min.vector
+    diss.matrix[ , to.keep.i ] <- min.vector
+    diss.matrix <- diss.matrix[ -to.remove.i, -to.remove.i ]
+
+    # Update the cluster list to reflect that the two points are now clustered
+    cluster.list[[ to.keep.i ]] <- c( cluster.list[[ to.keep.i ]],
+                                      cluster.list[[ to.remove.i ]] )
+    cluster.list <- cluster.list[ -to.remove.i ]
+  }
+
+  cluster.vector <- c()
+
+  # Compute the return vector
+  for( list.index in 1:length(cluster.list) ){
+    cluster.vector[ cluster.list[[list.index]] ] <- list.index
+  }
+
+  return( cluster.vector )
+}
---
+++ R/KMEANS.R
@@ -0,0 +1,114 @@
+# Description - Performs basic k-means clustering on a data matrix
+# Arguments - data.matrix : a numeric data matrix to perform clustering on
+#             K : the number of clusters
+# Returns - a list which has the following elements
+#             cluster: a vector of integers (from 1 to k) indicating the cluster
+#                      to which each row of the data matrix is associated
+#             centers: a matrix of K cluster centers
+#             withinss: a vector of the within-cluster sum squares for each
+#                       cluster
+#             tot.withinss: the sum of withinss for all clusters
+#             size: a vector of integers indicating the number of points
+#                   associated with each cluster
+#             iter: the number of outer iterations the function took to converge
+KMEANS <- function( data.matrix, K ){
+
+  # Initialize K clustering centers
+  centers <- matrix( nrow=K, ncol=ncol( data.matrix ) )
+
+  centers.index <- 1
+  # Randomly select K rows from the data.matrix to be the starting clusters
+  for( sample.index in sample( x=c( 1:nrow( data.matrix ) ), size=K ) ) {
+    centers[ centers.index, ] <- data.matrix[ sample.index, ]
+    centers.index <- centers.index + 1
+  }
+
+  # Initialize cluster vector
+  # This will store the index of the clustering center each row in the matrix
+  # is associated with
+  clusters <- c()
+
+  # Initialize outer loop iteration counter
+  iter <- 0
+
+  moved.center <- TRUE
+
+  while( moved.center ) {
+    iter <- iter + 1
+    moved.center <- FALSE
+
+    # Initialize a clear association list
+    # This will store matrices of all of the vectors associated with a clustering center
+    associations <- rep( list(NULL), K)
+
+    # For each data point in the data.matrix
+    for( row.i in 1:nrow( data.matrix ) ) {
+      # Find the closest clustering center to the point
+      nearest.center <- 0
+      distance <- -1
+
+      for( center.i in 1:K ){
+        temp.dist <- dist( rbind( centers[center.i, ], data.matrix[ row.i, ] ),
+                           method="euclidean" )
+        if( distance == -1 || temp.dist < distance ){
+          nearest.center <- center.i
+          distance <- temp.dist
+        }
+      }
+
+      # Assign that point to that clustering center
+      clusters[row.i] <- nearest.center
+
+      # Add that point to the association matrix
+      if( is.null(associations[[nearest.center]]) ) {
+        associations[[nearest.center]] <-
+          matrix( data.matrix[ row.i, ], ncol=ncol( data.matrix ) )
+      } else {
+        associations[[nearest.center]] <-
+          rbind( associations[[nearest.center]], data.matrix[ row.i, ] )
+      }
+    }
+
+
+    # Calculate the mean of all points in each cluster
+    for( center.i in 1:K ){
+      # If this mean is the same as the coordinates of the cluster's center,
+      # we're done
+      # Otherwise, move the cluster's center to the mean and repeat
+      cluster.mean <- colMeans( associations[[center.i]] )
+      if( !all( cluster.mean == centers[center.i, ] ) ){
+        centers[center.i, ] <- cluster.mean
+        moved.center <- TRUE
+      }
+    }
+  }
+
+  # Calculate the return data
+
+  # Calculate within point scatter (withinss)
+  withinss <- c()
+  tot.withinss <- 0
+
+  for( center.i in 1:K ){
+
+    center.withinss.total <- 0
+
+    for( row.i in 1:nrow( associations[[center.i]] ) ){
+      center.withinss.total <-
+        center.withinss.total + ( dist( rbind( centers[center.i, ], associations[[center.i]][ row.i, ] ),
+                                        method="euclidean" ) )^2
+    }
+
+    withinss[[center.i]] <- center.withinss.total
+    # Sum to find total within point scatter (tot.withinss)
+    tot.withinss <- tot.withinss + center.withinss.total
+  }
+
+  # Size, the number of points in each cluster, will just be the number of rows in
+  # the association matrix for each center
+  size <- lapply( associations, nrow )
+
+  return.list <- list( cluster=clusters, centers=centers, withinss=withinss,
+                       tot.withinss=tot.withinss, size=size, iter=iter)
+  return( return.list )
+}
